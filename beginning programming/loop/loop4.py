2d11014376c952c2d7caa4ec8e8840750bbf489a
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-08 14:30:58 -0500

    ICS20 Loop Exercises

---
+++ beginning programming/loop/loop4.py
@@ -0,0 +1,10 @@
+# Name Kaisen
+# Date November 8, 2021
+# Title Loop4
+# Description
+
+true = True
+while (true):
+    x = int(input('Please enter the first number: '))
+    y = int(input('Please enter the second number: '))
+    print(x + y)
