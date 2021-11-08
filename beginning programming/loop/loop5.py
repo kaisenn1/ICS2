cd6c6a3f3904ec1479eafdb7e626af497c791138
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-08 14:33:31 -0500

    ICS20 Loop Exercises

---
+++ beginning programming/loop/loop5.py
@@ -0,0 +1,11 @@
+# Name Kaisen
+# Date November 8, 2021
+# Title Loop4
+# Description
+
+true = True
+while (true):
+    x = int(input('Please enter the first number: '))
+    y = 0
+    x = y
+    print('The sum is', x + y)
