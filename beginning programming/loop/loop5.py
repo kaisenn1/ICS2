1d3146387f0f6b5aacaf2390e85e4f8986b64bf0
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-09 12:27:55 -0500

    ICS20 Loop Exercises

--- beginning programming/loop/loop5.py
+++ beginning programming/loop/loop5.py
@@ -1,11 +1,23 @@
 # Name Kaisen
 # Date November 8, 2021
-# Title Loop4
+# Title Loop5
 # Description
 
+import time
+
 true = True
+y = False
+print('Please enter a number, followed by another(s) to find the total sum')
+time.sleep(0.5)
 while (true):
-    x = int(input('Please enter the first number: '))
-    y = 0
-    x = y
-    print('The sum is', x + y)
+    x = int(input('Please enter a number: '))
+    if y == False:
+        y = 0
+        print('The value is',x)
+        y = x
+        z = x
+    else:
+        z = x + y
+        print('The total sum is', z)
+        y = z
+
