969739fdabb035330677af780c79004cfcd103c6
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-10 13:29:53 -0500

    ICS20 String Exercises

---
+++ beginning programming/strings/string1.py
@@ -0,0 +1,8 @@
+#Name Kaisen
+#Date Novemeber 8, 2021
+#Title String1
+#Description
+
+first = input('Please enter your first name: ')
+surname = input('Please enter your surname: ')
+print(first + ', ' + surname)
