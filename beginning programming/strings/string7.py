dcc36fd43316cbaec97bd1785e05df6ec552fa3a
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-12 12:58:08 -0500

    ICS20 String Exercises

---
+++ beginning programming/strings/string7.py
@@ -0,0 +1,10 @@
+#Name Kaisen
+#Date Novemeber 8, 2021
+#Title String7
+#Description
+
+count = 0
+while count < 5:
+    word = input('Please enter word: ')
+    count += 1
+
---
+++ beginning programming/strings/string8.py
@@ -0,0 +1,12 @@
+#Name Kaisen
+#Date Novemeber 8, 2021
+#Title String8
+#Description
+
+count = 0
+while count < 5:
+    names = input('Please enter names, to a maxiumum of 5: ')
+    count += 1
+    if count == 5 or names == 'end':
+        break
+
