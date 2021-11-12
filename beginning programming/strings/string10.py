49fea2fd8077d5a2c0caafa6986743ffd6b5fdc2
kaisenni <348687120@gapps.yrdsb.ca>
2021-11-12 13:10:06 -0500

    ICS20 String Exercises

---
+++ beginning programming/strings/string10.py
@@ -0,0 +1,14 @@
+#Name Kaisen
+#Date Novemeber 8, 2021
+#Title String8
+#Description
+
+
+while (True):
+    number = input('Please enter a integer: ')
+    if number == 'quit':
+        numbers = int(input('Please enter a integer greater than or equal to 10: '))
+        if numbers >= 10:
+            break
+        continue
+
---
+++ beginning programming/strings/string9.py
@@ -0,0 +1,11 @@
+#Name Kaisen
+#Date Novemeber 8, 2021
+#Title String8
+#Description
+
+
+while (True):
+    number = int(input('Please enter a integer: '))
+    if number > 20:
+        break
+
