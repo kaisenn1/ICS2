da39c72f0c45c0d800520ea513984c4264b513cf
kaisenni <348687120@gapps.yrdsb.ca>
2021-12-07 12:41:47 -0500

    ICS20 String Exercises

--- beginning programming/graphics/graphic11.py
+++ beginning programming/graphics/graphic11.py
@@ -6,11 +6,23 @@
 import pygame
 from pygame.locals import *
 
+black = (0, 0, 0)
+gray = (127, 127, 127)
+white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+cyan = (0, 255, 255)
+magenta = (255, 0, 255)
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
+
+print('Choose two colours from the below list to set strip colours')
+print('Black, Gray, White, Red, Green, Blue, Yellow, Cyan, Magenta')
+colour = input('Input first colour name: ')
+colour2 = input('Input second colour name: ')
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -20,9 +32,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.draw.rect(screen, red,(0, 0,160,480))
-    pygame.draw.rect(screen, green,(160, 0,160,480))
-    pygame.draw.rect(screen, red,(320, 0,160,480))
-    pygame.draw.rect(screen, green,(480, 0,160,480))
+    pygame.draw.rect(screen, colour,(0, 0,160,480))
+    pygame.draw.rect(screen, colour2,(160, 0,160,480))
+    pygame.draw.rect(screen, colour,(320, 0,160,480))
+    pygame.draw.rect(screen, colour2,(480, 0,160,480))
     pygame.display.update()
 pygame.quit()
