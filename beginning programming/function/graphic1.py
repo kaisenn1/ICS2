d7f785a0daca977572eaeb008617f2b0265a20e3
kaisenni <348687120@gapps.yrdsb.ca>
2021-12-10 12:52:20 -0500

    ICS20 Function Exercises

---
+++ beginning programming/function/graphic1.py
@@ -0,0 +1,30 @@
+#Name Kaisen
+#Date December 10, 2021
+#Title function1
+#Description
+
+'''
+pseudocode:
+ask for weight
+ask if special delivery is wanted
+pass weight and if special delivery is wanted into function
+calculate price of first gram + after first grame price * weight - 1
+if not whole{
+    add 35 cents
+}
+return price
+'''
+
+#no need to set variables, only one variable that is static
+def calccost(weight, special_delivery):
+    price = 1.05 + (0.35 * (round(weight) - 1)) #sets base price, then multiples gram cost by weight, removing 1 gram from weight because of inital cost
+    if weight != round(weight): #tests if weight is rounded, if weight is not equal to its rounded version, it means it is a partial gram and needs cost added
+        price += 0.35
+    if special_delivery.lower() == 'yes': #converts to lowercase in case user types upercase
+        price += 15
+    return price
+
+print('First-Class Letter Cost Calculator')
+weight = float(input('Please enter envelope weight in grams: ')) #takes wieght
+special_delivery = input('Is special delivery required?(Yes/No): ') #takes answer
+print('$' + str(calccost(weight,special_delivery))) #executes function, then converts function response to string, to be concatnated with "$"
