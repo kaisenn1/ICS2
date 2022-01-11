d1ec7f4449644da8a7d4daf53ecf480a0c0bf21a
Kaisen <348687120@gapps.yrdsb.ca>
2022-01-11 12:34:29 -0500

    ICS2 List Exercises

--- beginning programming/pixel/pixel2.py
+++ beginning programming/pixel/pixel2.py
@@ -1,7 +1,7 @@
 #Name Kaisen
 #Date Novemeber 26, 2021
 #Title pixel1
-#Description red line 100 pixels long, fron 15,10
+#Description vertical red line
 
 import pygame
 
@@ -20,6 +20,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill(white)
-    pygame.draw.line(screen,red,(15,10),(115,10))
+    pygame.draw.line(screen,red,(15,10),(15,60))
     pygame.display.update()
 pygame.quit()
--- beginning programming/pixel/pixel3.py
+++ beginning programming/pixel/pixel3.py
@@ -1,12 +1,12 @@
 #Name Kaisen
 #Date Novemeber 26, 2021
 #Title pixel1
-#Description red line 100 pixels long, fron 15,10
+#Description bored face
 
 import pygame
 
 white = (255,255,255)
-red = (255,0,0)
+black = (0, 0, 0)
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
@@ -20,6 +20,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill(white)
-    pygame.draw.line(screen,red,(15,10),(115,10))
+    pygame.draw.rect(screen, black,(200, 200,5,5))
+    pygame.draw.rect(screen, black,(435, 200,5,5))
+    pygame.draw.line(screen,black,(220,320),(420,320))
     pygame.display.update()
 pygame.quit()
