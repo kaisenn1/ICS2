a8aad957e43143a2ac962e6f602487423220c39c
Kaisen <348687120@gapps.yrdsb.ca>
2022-01-21 14:19:40 -0500

    ICS2 GAME

--- beginning programming/shapematchgame/game.py
+++ beginning programming/shapematchgame/game.py
@@ -1,54 +1,63 @@
 from setup import *
 import random
-pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 star = pygame.image.load('newstar.png')
 rectangle = pygame.image.load('rectangle.png')
 shapes = [rectangle, star]
 
 class Cooldown():
-    def __init__(self):
-        self.cooltime = 2000
-        self.cooltime1 = 250
-        self.copy = star.get_rect()
-        self.oldtime = pygame.time.get_ticks()
-    def random1(self):
-        num = (random.randint(100, 600), random.randint(100, 400))
-        return num
 
+    def __init__(self):
+        self.score = 0
 
     def randShape(self):
         image = shapes[random.randint(0, 1)]
         rect = image.get_rect()
         screen.fill(white)
         pygame.display.update()
-        rect.center = self.random1()
+        rect.center = (random.randint(100, 600), random.randint(100, 400))
         screen.blit(image, rect)
         pygame.display.update()
-        self.copy = rect.copy()
-        return self.copy
+        self.shapecopy = rect.copy()
+        return self.shapecopy
 
-
-    def test(self):
-        x = self.copy
-        if x.collidepoint(pygame.mouse.get_pos()):
-            print('ye')
+    def clicktest(self):
+        collider = self.shapecopy
+        for event in pygame.event.get():
+            if collider.collidepoint(pygame.mouse.get_pos()) and event.type == pygame.MOUSEBUTTONDOWN:
+                self.score += 1
+                screen.fill(white)
+                self.scoreboard()
+                pygame.display.update()
+    def scoreboard(self):
+        sysfont = pygame.font.get_default_font()
+        font = pygame.font.SysFont(sysfont, 50)
+        text = font.render('Score:'+str(self.score), 1, black)
+        center = text.get_rect(center=(75,20))
+        screen.blit(text,center)
 testing = Cooldown()
-running = True
+
 screen.fill(white)
-cooltime = 1000
+cooltime = 3000
 oldtime = pygame.time.get_ticks()
-x = None
+running = True
+pygame.init()
+p = 0
 while (running):
-    time = pygame.time.get_ticks()
-    if time - oldtime >= cooltime:
-        oldtime = time
-        if testing.randShape().collidepoint(pygame.mouse.get_pos()):
-            print('ye')
-    testing.test()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    time = pygame.time.get_ticks()
+    if time - oldtime >= cooltime:
+        oldtime = time
+        testing.randShape()
+        p = 1
+    if p == 1:
+        testing.clicktest()
+
+
 
 pygame.quit()
