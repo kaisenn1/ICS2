5476a0b581b8422903492de0249f37c6f7ca3893
Kaisen <348687120@gapps.yrdsb.ca>
2022-01-21 13:43:21 -0500

    ICS2 GAME

--- beginning programming/shapematchgame/game.py
+++ beginning programming/shapematchgame/game.py
@@ -10,38 +10,42 @@ class Cooldown():
     def __init__(self):
         self.cooltime = 2000
         self.cooltime1 = 250
+        self.copy = star.get_rect()
         self.oldtime = pygame.time.get_ticks()
     def random1(self):
         num = (random.randint(100, 600), random.randint(100, 400))
         return num
 
-    score1 = 0
+
     def randShape(self):
         image = shapes[random.randint(0, 1)]
         rect = image.get_rect()
-        copy = rect.copy()
-        copy.center = self.random1()
         screen.fill(white)
         pygame.display.update()
         rect.center = self.random1()
         screen.blit(image, rect)
         pygame.display.update()
+        self.copy = rect.copy()
+        return self.copy
 
-        return copy
 
     def test(self):
-        if self.randShape().collidepoint(pygame.mouse.get_pos()):
+        x = self.copy
+        if x.collidepoint(pygame.mouse.get_pos()):
             print('ye')
 testing = Cooldown()
 running = True
 screen.fill(white)
-cooltime = 5000
+cooltime = 1000
 oldtime = pygame.time.get_ticks()
+x = None
 while (running):
     time = pygame.time.get_ticks()
     if time - oldtime >= cooltime:
         oldtime = time
-        testing.test()
+        if testing.randShape().collidepoint(pygame.mouse.get_pos()):
+            print('ye')
+    testing.test()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
